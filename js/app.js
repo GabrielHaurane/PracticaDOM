1574135f34651486041fc9e306a7f5235056549d
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-07-24 11:13:47 -0300

    agregamos crearParrafo

--- js/app.js
+++ js/app.js
@@ -8,7 +8,23 @@ const cambiarTitulo = () => {
     const tituloPrincipal = document.getElementById(`titulo`);
     console.log(tituloPrincipal);
 
-    // cambiar ese h1, texto y style
     // tituloPrincipal.innerText = `Cambiamos el titulo JS`
     tituloPrincipal.innerHTML = `Cambiamos el titulo  <strong>JS</strong>`;
+    tituloPrincipal.className = `text-info display-4`;
+    // cambiar ese h1, texto y style
 }
+const crearParrafo = () =>{
+    console.log(`desde crearParrafo`)
+    // crear un nuevo parrafo
+
+    // insertar el nuevo parrafo en el segundo section
+}
+
+// agregar un evento a un elemento de html
+const btnVerMas = document.querySelector(`.btn-outline-info`);
+console.log(btnVerMas);
+
+// manejadores de eventos 
+// cuando tengo que invocar una funcion con parametros es con una funcion anonima o arrow
+// btnVerMas.addEventListener(`click`, () => crearParrafo(p1))
+btnVerMas.addEventListener(`click`,crearParrafo);
